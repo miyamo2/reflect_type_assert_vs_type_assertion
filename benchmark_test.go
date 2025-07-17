a7aa327b47ae9a151a49d1c11eb4c4bda6992d6e
miyamo2 <miyamo2@outlook.com>
2025-07-18 04:15:12 +0900

    add benchmark test

---
+++ benchmark_test.go
@@ -0,0 +1,29 @@
+package reflect_type_assert_vs_type_assertion
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+func BenchmarkTypeAssertion(b *testing.B) {
+	rv := reflect.ValueOf(new(time.Time)).Elem()
+	b.ReportAllocs()
+	for b.Loop() {
+		_, ok := rv.Interface().(time.Time)
+		if !ok {
+			b.Fatal("expected true but got false")
+		}
+	}
+}
+
+func BenchmarkReflectTypeAssert(b *testing.B) {
+	rv := reflect.ValueOf(new(time.Time)).Elem()
+	b.ReportAllocs()
+	for b.Loop() {
+		_, ok := reflect.TypeAssert[time.Time](rv)
+		if !ok {
+			b.Fatal("expected true but got false")
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/miyamo2/reflect_type_assert_vs_type_assertion
+
+go 1.25rc2
+
+tool golang.org/x/perf/cmd/benchstat
+
+require (
+	github.com/aclements/go-moremath v0.0.0-20210112150236-f10218a38794 // indirect
+	golang.org/x/perf v0.0.0-20250710210952-7b7c2de18447 // indirect
+)
